88e4972bab039c57e8572032fb9c60a8b1038513
Olivier HABIMANA <habimanaolivier6@gmail.com>
2021-07-01 08:25:59 +0200

    add first init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import queryString from "query-string";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+const App = () => {
+  let { search } = useLocation();
+
+  const { url } = queryString.parse(search);
+
+  return (
+    <Switch>
+      <Route path="/" render={() => (window.location = url)} />
+    </Switch>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
